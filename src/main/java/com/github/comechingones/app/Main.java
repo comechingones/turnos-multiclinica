f1a5d3fc0e1b16acb9360cc43cc020374e7cfbaa
aformen <aguformenti01@gmail.com>
2025-08-11 20:38:14 -0300

    feat: proyecto base Gradle + Hola Mundo

---
+++ src/main/java/com/github/comechingones/app/Main.java
@@ -0,0 +1,12 @@
+package com.github.comechingones.app;                  // ubica la clase en el paquete
+
+public class Main {                                     // clase principal
+    public static void main(String[] args) {            // punto de entrada del programa
+        System.out.println("Sistema de Turnos Médicos - Comechingones v0.1"); // salida por consola
+    }
+}
+// Línea por línea (rápido):
+//        package ...; → organiza la clase dentro del paquete.
+//        public class Main { → definís una clase pública llamada Main.
+//        public static void main(String[] args) → método que Java ejecuta primero.
+//        System.out.println(...) → imprime texto en la consola.
